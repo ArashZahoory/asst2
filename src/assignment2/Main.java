97b2f9da3e52e9735d1e7f8ad660efc9dc60fe83
Arash Zahoory <arashzahoory@gmail.com>
2014-02-19 14:53:38 -0800

    assignment2 first commit

---
+++ src/assignment2/Main.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package assignment2;
+
+
+import java.util.*;
+
+/**
+ *
+ * @author arashzahoory
+ */
+public class Main
+{
+    public static void main (String [] args)
+    {
+        ArrayList<Process> processArrayList = new ArrayList<Process>();
+        ProcessGenerator newProcesses = new ProcessGenerator(10);
+        processArrayList = newProcesses.generateProcesses();
+
+
+
+
+
+    }
+}
+
